12ff2a4cdb50c5cf7547341cb64314ae6f655e94
Ganesh Bhargav <96660473+Ganeshbhargav@users.noreply.github.com>
2025-04-21 18:15:54 +0530

    Add files via upload

---
+++ Color Picker/script.js
@@ -0,0 +1,28 @@
+function color1()
+{
+    document.getElementById("container").style.backgroundColor="#e0e0e0";
+    document.getElementById("backgroundColorChange").textContent="#e0e0e0";
+    document.getElementById("backgroundColorChange").style.color="#e0e0e0";
+
+}
+function color2()
+{
+    document.getElementById("container").style.backgroundColor="#6fcf97";
+    document.getElementById("backgroundColorChange").textContent="#6fcf97";
+    document.getElementById("backgroundColorChange").style.color="#6fcf97";
+
+}
+function color3()
+{
+    document.getElementById("container").style.backgroundColor="#56ccf2";
+    document.getElementById("backgroundColorChange").textContent="#56ccf2";
+    document.getElementById("backgroundColorChange").style.color="#56ccf2";
+
+}
+function color4()
+{
+    document.getElementById("container").style.backgroundColor="#bb6bd9";
+    document.getElementById("backgroundColorChange").textContent="#bb6bd9";
+    document.getElementById("backgroundColorChange").style.color="#bb6bd9";
+
+}
